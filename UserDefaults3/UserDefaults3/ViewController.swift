915b0b7424f7b2ab30f5b43c33a6cb033f9e6f5b
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-15 22:24:25 +0900

    ボタンをViewControllerと関連づける

--- UserDefaults3/UserDefaults3/ViewController.swift
+++ UserDefaults3/UserDefaults3/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         self.saveCurrentTime()
     }
     
