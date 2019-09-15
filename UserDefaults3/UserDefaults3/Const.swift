4dc906df218694c60dad8de6e5af69694795d604
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-15 23:24:05 +0900

    定数クラスを追加

---
+++ UserDefaults3/UserDefaults3/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  UserDefaults3
+//
+//  Created by home on 2019/09/15.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class Const: NSObject {
+    
+}
