3ed7df8478dfe01f4f25e84183f915ab93d4a4e7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-15 22:43:08 +0900

    現在時刻を保存する処理を日時を管理するファイルに実装し直す

--- UserDefaults3/UserDefaults3/DateTimeManeger.swift
+++ UserDefaults3/UserDefaults3/DateTimeManeger.swift
@@ -9,5 +9,15 @@
 import Foundation
 
 class DateTimeManeger: NSObject {
-    
+    // 現在時刻を保存
+    func saveCurrentTime() {
+        let dateFormatter =  DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        let now = Date()
+        
+        let defaults = UserDefaults.standard
+        defaults.set(dateFormatter.string(from: now), forKey: "currentTime")
+    }
 }
