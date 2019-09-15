40b70e215d563ad9b11939218d0b279cb1080f8f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-15 22:19:46 +0900

    現在時刻を保存

--- UserDefaults3/UserDefaults3/ViewController.swift
+++ UserDefaults3/UserDefaults3/ViewController.swift
@@ -10,36 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
-    @IBAction func getList(_ sender: Any) {
-        // ユーザーデフォルトを参照する
-        let defaults = UserDefaults.standard
-        // キー"myList"の値を配列として読み込む
-        if let theList = defaults.array(forKey: "myList") {
-            print(theList)
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 現在時刻を保存
+        let dateFormatter =  DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        let now = Date()
         
-        //  配列を作る
-        let list = [1,2,3]
-        // ユーザーデフォルトを参照する
         let defaults = UserDefaults.standard
-        // 配列listをキー"myList"で保存する
-        defaults.set(list, forKey: "myList")
-        
+        defaults.set(dateFormatter.string(from: now), forKey: "currentTime")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-
 }
 
