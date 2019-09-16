408afe9100bd23d15f07d3ea5e48f733b7d658dc
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-16 22:25:22 +0900

    通知を登録する処理を追加

--- UserDefaults3/UserDefaults3/Sources/Models/NotificationObserver.swift
+++ UserDefaults3/UserDefaults3/Sources/Models/NotificationObserver.swift
@@ -9,5 +9,12 @@
 import Foundation
 
 class NotificationObserver: NSObject {
+    func addObserver() {
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(notified(notification:)), name: .UIApplicationDidBecomeActive, object: nil)
+    }
     
+    @objc private func notified(notification: Notification) {
+        print("呼ばれた")
+    }
 }
