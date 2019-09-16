57dec8bd5fd0b4e74a53beacbe5f3b7070ce48ba
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-16 22:03:19 +0900

    ContainerViewのカスタムクラスを設定

---
+++ UserDefaults3/UserDefaults3/Sources/Controllers/ContainerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerViewController.swift
+//  UserDefaults3
+//
+//  Created by home on 2019/09/16.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
