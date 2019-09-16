ba11088759871bb340fa81219890696d80b1c8f7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-16 22:05:53 +0900

    現在時刻を保存する処理をContainerViewControllerクラスに実装し直す

--- UserDefaults3/UserDefaults3/Sources/Controllers/ContainerViewController.swift
+++ UserDefaults3/UserDefaults3/Sources/Controllers/ContainerViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ContainerViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // 現在時刻を保存
+        let currentTime = DateTime().fetchCurrentTime()
+        let defaults = UserDefaults.standard
+        defaults.set(currentTime, forKey: UserDefaultConst.currentTime)
     }
 }
