304c4a5082e28b82ca1b118fda957e9d9c7b3be0
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-16 22:18:19 +0900

    NotificationCenterを扱うクラスを追加

---
+++ UserDefaults3/UserDefaults3/Sources/Models/NotificationObserver.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationObserver.swift
+//  UserDefaults3
+//
+//  Created by home on 2019/09/16.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import Foundation
+
+class NotificationObserver: NSObject {
+    
+}
