7f4026fce6ea1677b90f94c9c406cb486bfc0746
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-09-17 10:49:13 +0900

    現在時刻が前回保存した時刻より先の場合はボタンを表示する

--- UserDefaults3/UserDefaults3/Sources/Controllers/ContainerViewController.swift
+++ UserDefaults3/UserDefaults3/Sources/Controllers/ContainerViewController.swift
@@ -12,15 +12,38 @@ class ContainerViewController: UIViewController {
     
     @IBOutlet weak var button: UIButton!
     
-    let notificationObserver = NotificationObserver()
+    //    let notificationObserver = NotificationObserver()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.notificationObserver.addObserver()
+        //        self.notificationObserver.addObserver()
+        self.addObserver()
         
         // 現在時刻を保存
         let currentTime = DateTime().fetchCurrentTime()
         let defaults = UserDefaults.standard
         defaults.set(currentTime, forKey: UserDefaultConst.currentTime)
+    }
+    
+    private func addObserver() {
+        let center = NotificationCenter.default
+        center.addObserver(self, selector: #selector(type(of: self).notified(notification:)), name: .UIApplicationDidBecomeActive, object: nil)
+        print("通知を登録")
+    }
+    
+    @objc private func notified(notification: Notification) {
+        // 現在時刻を取得
+        let currentTime = DateTime().fetchCurrentTime()
+        print("現在時刻を取得\(currentTime)")
+        // 前回保存した時刻を呼び出す
+        let defaults = UserDefaults.standard
+        guard let lastSavedTime = defaults.string(forKey: UserDefaultConst.currentTime) else {
+            print("処理を中断")
+            return
+        }
+        print("前回保存した時刻を取得\(lastSavedTime)")
+        if currentTime > lastSavedTime {
+            self.button.isHidden = false
+        }
     }
 }
--- UserDefaults3/UserDefaults3/Sources/Models/NotificationObserver.swift
+++ UserDefaults3/UserDefaults3/Sources/Models/NotificationObserver.swift
@@ -9,13 +9,25 @@
 import Foundation
 
 class NotificationObserver: NSObject {
-    func addObserver() {
-        let center = NotificationCenter.default
-        center.addObserver(self, selector: #selector(type(of: self).notified(notification:)), name: .UIApplicationDidBecomeActive, object: nil)
-        print("通知を登録")
-    }
-    
-    @objc private func notified(notification: Notification) {
-        print("呼ばれた\(notification)")
-    }
-}
+//    func addObserver() {
+//        let center = NotificationCenter.default
+//        center.addObserver(self, selector: #selector(type(of: self).notified(notification:)), name: .UIApplicationDidBecomeActive, object: nil)
+//        print("通知を登録")
+//    }
+//    
+//    @objc private func notified(notification: Notification) {
+//        // 現在時刻を取得
+//        let currentTime = DateTime().fetchCurrentTime()
+//        print("現在時刻を取得\(currentTime)")
+//        // 前回保存した時刻を呼び出す
+//        let defaults = UserDefaults.standard
+//        guard let lastSavedTime = defaults.string(forKey: UserDefaultConst.currentTime) else {
+//            print("処理を中断")
+//            return
+//        }
+//        print("前回保存した時刻を取得\(lastSavedTime)")
+//        if currentTime >= lastSavedTime {
+//            ContainerViewController().button.isHidden = false
+//        }
+//    }
+//}
